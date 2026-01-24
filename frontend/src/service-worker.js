eba12ce162b1385ab37cea55c59b5bcab87b81de
Vladimir Burenko <funquenop@gmail.com>
2026-01-24 14:48:15 +0500

    po pwa

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -97,7 +97,7 @@ self.addEventListener('notificationclick', function (event) {
     const urlToOpen = event.notification.data?.url || '/';
 
     event.waitUntil(
-        clients.matchAll({
+        self.clients.matchAll({
             type: 'window',
             includeUncontrolled: true
         }).then(function (windowClients) {
@@ -115,8 +115,8 @@ self.addEventListener('notificationclick', function (event) {
                 }
             }
             // If not, open a new window
-            if (clients.openWindow) {
-                return clients.openWindow(urlToOpen);
+            if (self.clients.openWindow) {
+                return self.clients.openWindow(urlToOpen);
             }
         })
     );
