d7335a74d8b0483262777d8a84472b6f4c31e204
Vladimir Burenko <funquenop@gmail.com>
2026-02-13 23:31:13 +0500

    update & bugfix

--- frontend/src/components/pages/ChatPage.jsx
+++ frontend/src/components/pages/ChatPage.jsx
@@ -28,6 +28,7 @@ import AttachmentModal from '../modals/AttachmentModal';
 import Rating from './Rating';
 import { downloadFile } from '../../services/downloadService';
 import ResolveConfirmationModal from '../modals/ResolveConfirmationModal';
+import Loader from '../shared/Loader';
 import DefaultAvatarIcon from '../shared/DefaultAvatarIcon';
 import axios from 'axios';
 import { useReadOnlyCheck } from '../../hooks/useReadOnlyCheck';
@@ -1197,10 +1198,7 @@ const ChatPage = () => {
                         className="bg-indigo-600 text-white rounded-full p-2 hover:bg-indigo-700 disabled:bg-indigo-300 disabled:cursor-not-allowed transition-colors self-end relative"
                       >
                         {isSending ? (
-                          <svg className="animate-spin h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                          </svg>
+                          <Loader size="h-6 w-6" color="text-white" />
                         ) : (
                           editingMessage ? <CheckBadgeIcon className="h-6 w-6" /> : <ArrowUpCircleIcon className="h-6 w-6" />
                         )}
